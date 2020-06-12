0db12e930595b0540f2d78f6c4d43c2979ca4afa
Abel C Dixon <abelcheruvathoor@gmail.com>
2020-06-12 10:37:33 +0530

    bug fixes and work with all test cases

--- window_slide.c
+++ window_slide.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int isPrime(int n){
-    int c;
     if (n==1){
         return 0;
     }
-    for ( c = 2 ; c <= n - 1 ; c++ )
-    {
-        if ( n%c == 0 )
+    for (int i = 2 ; i < n ; i++ ){
+        if ( n%i == 0 )
             return 0;
     }
     return 1;
@@ -27,7 +25,7 @@ int minWindowPeriod(int m,int n,int p){
     for(slider=1;slider<(n-m+1);slider++){
 
         int count[10],k=0,flag=0;
-        while(k<((n-m+1-slider)+1)){
+        while(k<=((n-m+1-slider)+1)){
             int window[10],z=0;
             for (int j = k; j<slider+k; ++j) {
                 window[z]=arr[j];
